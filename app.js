385beadfdc3cc1cc931d2b2a7b936bab97ee2c00
Sushant2 <er.sushant.2@gmail.com>
2020-05-15 20:34:45 +0530

    data files

---
+++ app.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=New+Delhi,IN&APPID=89550832828349a7b738892e33be75fc&units=metric;';
+
+var server = http.createServer(function(request, response){
+    //All logic will go here
+    var request = require('request');
+    request(url, function(err, res, body){
+        var data = JSON.parse(body);
+        response.write("<html><body><div id='container'>");
+            response.write("<h1>"+ 'City Name -: ' + data['name'] + '<br>' + "</h1>");
+            response.write("<h2>"+ 'Temperature -: ' + data.main['temp'] + '<br>' + "</h2>");
+            response.write("<h2>"+'SunSet Time -: ' + new Date(data.sys['sunset']*1000)+"</h2>");
+            response.write("</div></body></html>");
+            response.end();
+    });
+}).listen(3000);
